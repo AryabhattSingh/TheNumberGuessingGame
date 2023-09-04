3d09e702ee0f833b106cf84d04c6ed228dfed24a
Aryabhatt <bhattsahab314@gmail.com>
2023-09-05 00:25:21 +0530

    Added a play() function. This will be the main function to driva game

--- game_functions.py
+++ game_functions.py
@@ -1,3 +1,6 @@
+import random
+import os
+
 
 def first_game_instructions():
     print("\nComputer will select a number in the range of 1 to 100.")
@@ -17,3 +20,32 @@ def print_game_instructions():
     print(">=1 and <3     -> Almost there")
 
 
+def set_difficulty():
+    pass
+
+
+def make_a_guess():
+    pass
+
+
+def evaluate_guess(random_selected_number, user_guess, lives):
+    pass
+
+
+def play_game():
+    print(f"\nWelcome to The Number Guessing Game!")
+    lives = set_difficulty()
+    print(f"\nCOMPUTER SELECTED A NUMBER IN THE RANGE OF 1 AND 100")
+    random_selected_number = random.randint(1, 100)
+
+    user_guess = 0
+    while user_guess != random_selected_number and lives > 0:
+        print(f"\nYou have {lives} attempts remaining to guess the number.")
+        user_guess = make_a_guess()
+        lives = evaluate_guess(random_selected_number, user_guess, lives)
+        if lives == 0:
+            print(f"\n{'*' * 36}")
+            print("You have run out of lives. Game over")
+            print(f"{'*' * 36}")
+        elif user_guess != random_selected_number:
+            print("Guess again!")
